2fec7d5b03c2eeea4db41e70a6f12bed256339e1
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-03-08 01:54:25 -0500

    works

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoItem from "./TodoItem"
+
+function TodoList({ todos, toggleComplete, deleteTodo, editTodo}) {
+     return (
+          <ul>
+               {todos.map(todo => (
+                    <TodoItem 
+                         key={todo.id}
+                         todo={todo}
+                         toggleComplete={toggleComplete}
+                         deleteTodo={deleteTodo}
+                         editTodo={editTodo}
+                         />
+               ))}
+          </ul>
+     );
+}
+
+
+export default TodoList;
